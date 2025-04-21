0d607ff5273d26492b8a85027597d529b9af2c46
lavaKangJun <young16505@gmail.com>
2025-04-22 01:04:28 +0900

    도시 검색 뷰 임시 구현

--- AddCity/AddCity/AddCityView.swift
+++ AddCity/AddCity/AddCityView.swift
@@ -9,11 +9,36 @@ import SwiftUI
 
 public struct AddCityView: View {
     @State private var textedCity: String = ""
+    @FocusState private var isSearchFocus: Bool
     
     public var body: some View {
-        VStack {
-            HStack {
-                TextField("도시 검색", text: $textedCity)
+        ZStack {
+            Color.clear
+                .contentShape(Rectangle())
+                .ignoresSafeArea()
+                .onTapGesture {
+                    isSearchFocus = false
+                }
+            
+            VStack {
+                Spacer()
+                    .frame(height: 20)
+                
+                HStack {
+                    TextField("도시 검색", text: $textedCity)
+                        .focused($isSearchFocus)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .padding(8)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                    
+                    Button("Cancel") {
+                        
+                    }
+                }
+                .padding(20)
+                
+                Spacer()
             }
         }
     }
